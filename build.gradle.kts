1538f17ed14871a28b26d1e32d292ca7ec821a59
Maksym Dybarskyi <maksym.dybarskyi@gymondo.de>
2021-12-23 17:21:43 +0100

    Upgrade kotlin and libs\

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	val kotlinVersion = "1.4.21"
+	val kotlinVersion = "1.6.10"
 	kotlin("jvm") version kotlinVersion
 	kotlin("kapt") version kotlinVersion
 	kotlin("plugin.serialization") version kotlinVersion
@@ -13,7 +13,6 @@ application {
 }
 
 repositories {
-	jcenter()
 	mavenCentral()
 	maven("https://kotlin.bintray.com/kotlinx")
 	maven("https://oss.sonatype.org/content/groups/public")
@@ -22,9 +21,9 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 
-	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
 	implementation("com.pi4j:pi4j-core:1.4")
 }
 
